195f9a018b978e3a1dc65345cec01f8ba0b6caa0
philip.zhou <philip.zhou.2009@gmail.com>
2014-10-13 18:05:55 +0800

    fdbapp02 initial import;

---
+++ src/main/java/com/fdb/android/fdbapp02/SplashActivity.java
@@ -0,0 +1,60 @@
+package com.fdb.android.fdbapp02;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread logoTimer = new Thread() {
+            public void run(){
+                try{
+                    int logoTimer = 0;
+                    while(logoTimer < 800){
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    startActivity(new Intent("com.fdb.android.fdbapp02.IntroActivity"));
+                }
+
+                catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                finally{
+                    finish();
+                }
+            }
+        };
+
+        logoTimer.start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
